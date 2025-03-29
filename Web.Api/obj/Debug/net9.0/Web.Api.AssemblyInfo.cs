9b27c288cc8b44fc2f154f79807788af70317ec4
Jonatas Valente <jntsvalente@gmail.com>
2025-03-29 03:01:50 -0300

    Modify Cache

--- Web.Api/obj/Debug/net9.0/Web.Api.AssemblyInfo.cs
+++ Web.Api/obj/Debug/net9.0/Web.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55168528f0a613e1bf838f7d61497e418b220897")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
